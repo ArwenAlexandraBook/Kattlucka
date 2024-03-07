a1b0b1347f21e9100a0cc796b6535fe26ef4172f
Arwen Alexandra <90157855+ArwenAlexandraBook@users.noreply.github.com>
2024-03-07 13:30:47 +0100

    Update led_control.cpp

--- src/led_control.cpp
+++ src/led_control.cpp
@@ -9,34 +9,30 @@ void LEDControl::initialize() {
 
 void LEDControl::turnOnGreen() {
     digitalWrite(GREEN_PIN, HIGH);
-    Serial.println("THE Green LED is on");
-    delay(15000); // Green LED is on for 15 seconds when the flap is open. 
+    Serial.println("Grön LED lyser");
+    greenStartTime = millis();
+}
+
+void LEDControl::checkGreen() {
+    if (millis() - greenStartTime >= 15000) {
+        turnOffGreen();
+    }
 }
 
 void LEDControl::turnOffGreen() {
     digitalWrite(GREEN_PIN, LOW);
-    Serial.println("The Green LED is off and the flap is closed");
+    Serial.println("Grön LED är släckt och luckan är stängd");
 }
 
 void LEDControl::turnOnRed() {
     digitalWrite(RED_PIN, HIGH);
-    Serial.println("Red LED is on. Failed to open the flap");
-    delay(2000);
-    Serial.println("The red LED is off. Please try again");
-    turnOffRed();
+    Serial.println("Röd LED, lyser luckan är stängd);
+    redStartTime = millis();
 }
 
-void LEDControl::turnOffRed() {
-    digitalWrite(RED_PIN, LOW);
-}
-
-void LEDControl::turnOnYellow() {
-    digitalWrite(YELLOW_PIN, HIGH);
-    Serial.println("Yellow LED is on check the battery level");
-    delay(5000);
-}
-
-void LEDControl::turnOffYellow() {
-    digitalWrite(YELLOW_PIN, LOW);
-    Serial.println("The yellow LED is off. Battery level has been checked");
+void LEDControl::checkRed() {
+    if (millis() - redStartTime >= 2000) {
+        Serial.println("Röd LED har släckts var god och prova igen");
+        turnOffRed();
+    }
 }
