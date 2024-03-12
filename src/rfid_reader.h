e54d80e904b26bc6327c210c1f04855ff037b155
Arwen Alexandra <90157855+ArwenAlexandraBook@users.noreply.github.com>
2024-03-12 16:19:01 +0100

    Update rfid_reader.h

--- src/rfid_reader.h
+++ src/rfid_reader.h
@@ -1,17 +1,31 @@
+/*
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      ESP32
+ *             Reader/PCD   
+ * Signal      Pin          Pin           
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST         GPIO22            
+ * SPI SS      SDA(SS)     GPIO21            
+ * SPI MOSI    MOSI        GPIO23            
+ * SPI MISO    MISO        GPIO19            
+ * SPI SCK     SCK         GPIO18
+ * GND         GND         GND            
+ * -----------------------------------------------------------------------------------------
+ */
+
 #ifndef RFID_READER_H
 #define RFID_READER_H
 
-class RFIDReader {
-public:
-    virtual void initialize() = 0;
-    virtual int readData(char *data, int bufferSize) = 0;
-};
+#include "MFRC522.h"
 
-class MFRC522RFIDReader : public RFIDReader {
+class MFRC522RFIDReader {
 public:
-    MFRC522RFIDReader();
-    void initialize() override;
-    int readData(char *data, int bufferSize) override;
+    MFRC522RFIDReader(MFRC522& mfrc522);
+    void initialize();
+    int readData(char *data, int bufferSize);
+
+private:
+    MFRC522& mfrc522;
 };
 
 #endif // RFID_READER_H
