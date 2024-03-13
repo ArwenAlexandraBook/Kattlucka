df685cbae403d3a54df67e149b2a069202eb3cf1
Arwen Alexandra <90157855+ArwenAlexandraBook@users.noreply.github.com>
2024-03-13 08:37:01 +0100

    Update led_control.h

--- src/led_control.h
+++ src/led_control.h
@@ -1,19 +1,28 @@
 #ifndef LED_CONTROL_H
 #define LED_CONTROL_H
 
-#define GREEN_PIN 2
-#define RED_PIN 3
-#define YELLOW_PIN 8
+// GPIO pins för LED-lamporna
+#define GREEN_PIN 12 
+#define RED_PIN 32
+#define YELLOW_PIN 33
 
 class LEDControl {
 public:
     void initialize();
     void turnOnGreen();
+    void checkGreen(); 
     void turnOffGreen();
     void turnOnRed();
+    void checkRed(); 
     void turnOffRed();
     void turnOnYellow();
+    void checkYellow(); 
     void turnOffYellow();
+
+private:
+    unsigned long greenStartTime; 
+    unsigned long redStartTime;
+    unsigned long yellowStartTime; 
 };
 
 #endif
